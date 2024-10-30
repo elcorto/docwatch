96917f44a8e4ab0a221a996f28dddb4cb0f32158
Steve Schmerler <git@elcorto.com>
2024-10-30 14:29:01 +0100

    ENH: log non-fail pandoc messages
    Pandoc prints warnings (on stderr), such as
    
        [WARNING] Missing character: There is no ✓ (U+2713) (U+2713) in font [NewCM10-Book.otf]/OT:script=
    
    Before we only catched stdout and stderr of the pandoc command fails.
    Now we also log messages when pandoc doesn't fail.

--- src/docwatch/subproc.py
+++ src/docwatch/subproc.py
@@ -6,25 +6,36 @@ import traceback
 from .conf import conf
 
 
-def run_cmd(cmd, onerror="log"):
+def write_to_logfile(txt: str):
+    stamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    txt = re.sub("^", stamp + ": ", txt, flags=re.M).strip() + "\n"
+    with open(conf["DEFAULT"]["logfile"], "a") as fd:
+        fd.write(txt)
+
+
+def run_cmd(cmd: str, onerror="log"):
     assert onerror in (
         valid := ["log", "fail"]
     ), f"{onerror=} not one of {valid}"
     try:
-        subprocess.run(
+        proc = subprocess.run(
             re.sub(r"\s{2,}", " ", cmd.strip(), flags=re.M),
             check=True,
             shell=True,
             stderr=subprocess.STDOUT,
             stdout=subprocess.PIPE,
         )
+        # If the pandoc call fails (exit !=0) then we catch this with
+        # check=True and handle the CalledProcessError below. But things like
+        # warning text on stderr goes silently to the logfile.
+        out = proc.stdout.decode()
+        if len(out) > 0:
+            write_to_logfile(out)
     except subprocess.CalledProcessError as ex:
         if onerror == "log":
-            out = ex.stdout.decode() + "\n" + traceback.format_exc()
-            stamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-            out = re.sub("^", stamp + ": ", out, flags=re.M).strip() + "\n"
-            with open(conf["DEFAULT"]["logfile"], "a") as fd:
-                fd.write(out)
+            write_to_logfile(
+                ex.stdout.decode() + "\n" + traceback.format_exc()
+            )
         elif onerror == "fail":
             print(ex.stdout.decode())
             raise ex
